bbdf29e9993324a939a0cc914fb4db7b1bb7a2be
Dmitry Sigaev <Dmitry_Sigaev@epam.com>
2016-09-07 17:52:28 +0300

    sandbox:gss cpp local experiments

--- gss/gss.cpp
+++ gss/gss.cpp
@@ -4,6 +4,11 @@
 #include <ostream>   // for output operator and std::endl
 #include <algorithm> // for std::copy
 #include <iterator>  // for std::output_iterator
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <future>
  
 // Function template max_subseq
 //
@@ -73,9 +78,80 @@ template<typename ForwardIterator>
 // the test array
 int array[] = { -1, -2, 3, 5, 6, -2, -1, 4, -4, 2, -1 };
  
-// function template to find the one-past-end pointer to the array
-template<typename T, int N> int* end(T (&arr)[N]) { return arr+N; }
- 
+
+using namespace std;
+
+struct S1 {};
+struct S2 {};
+
+template<typename X = S1>
+struct T;
+
+template<>
+struct T<S1> {
+	static void f() { cout << 1 << endl; }
+};
+
+template<>
+struct T<S2> {
+	static void f() { cout << 2 << endl; }
+};
+
+template< template<typename X = S2> class C>
+void g() {
+	C<>::f();
+}
+
+#include <iostream>
+uint8_t swap_bits(uint8_t byte_to_reverse)
+{
+	uint8_t b = byte_to_reverse;
+	b = ((b * 0x0802LU & 0x22110LU) | (b * 0x8020LU & 0x88440LU)) * 0x10101LU >> 16;
+	return b;
+}
+#define N 10
+int32_t average(int32_t m[N])
+{
+	int32_t sum = 0;
+	for (size_t i = 0; i < N; i++) {
+		sum += m[i];
+	}
+	return sum / N;
+}
+
+int main()
+{
+
+	int32_t n[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int32_t av = average(n);
+	float  a = 1 / 3., b = -1. / 7.;
+	float c = 3.;
+	float  i = 1 / 3., j = 1. / 7.;
+	float k = 3.;
+
+	if ((a + b)*c != (a*c + b*c))
+	{
+		printf("Distributivity is not always true");
+	}
+
+	if ((i + j)*k == (i*k + j*k))
+	{
+		printf("Distributivity is true");
+	}
+
+
+
+
+}
+
+
+/*
+int main() {
+	g<T>();
+	return 0;
+}
+*/
+ /*
 int main()
 {
   // find the subsequence
@@ -87,3 +163,4 @@ int main()
  
   return 0;
 }
+*/
