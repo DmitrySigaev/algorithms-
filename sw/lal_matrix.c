048769c09fa9397ff4e87440cd2a923c55f7d8b1
Dmitry Sigaev <Dmitry_Sigaev@epam.com>
2016-09-16 22:38:30 +0300

    redesign matrix structure

--- sw/lal_matrix.c
+++ sw/lal_matrix.c
@@ -1,54 +1,134 @@
 #include <stdio.h>
 #include <float.h>
 #include <malloc.h>
+#include <string.h>
 #include "lal_matrix.h"
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-matrix_t matrix(const size_t nrows, const size_t ncols)
+matrix_t matrix(const size_t nrows, const size_t ncols, enum MATTYPE type)
 {
-	double ** score_mat = (double **)malloc(nrows * sizeof(double *));
-	if (NULL == score_mat) {
-		return (matrix_t){ NULL, 0, 0 };
-	}
-
-	for (size_t i = 0; i < nrows; i++) {
-		score_mat[i] = (double *)malloc(ncols * sizeof(double));
-		if (NULL == score_mat[i]) {
-			for (size_t j = 0; j < i; j++) {
-				free(score_mat[j]);
-			}
-			free(score_mat);
-			return (matrix_t){ NULL, 0, 0 };
+	switch (type) {
+	case DOUBLETYPE: {
+		double ** score_mat = (double **)malloc(nrows * sizeof(double *));
+		if (NULL == score_mat) {
+			return (matrix_t) { NULL, 0, 0, VOIDTYPE };
 		}
+
+		for (size_t i = 0; i < nrows; i++) {
+			score_mat[i] = (double *)malloc(ncols * sizeof(double));
+			if (NULL == score_mat[i]) {
+				for (size_t j = 0; j < i; j++) {
+					free(score_mat[j]);
+				}
+				free(score_mat);
+				return (matrix_t) { NULL, 0, 0, VOIDTYPE };
+			}
+		}
+		return (matrix_t) { (double **)score_mat, nrows, ncols, DOUBLETYPE };
+	} break;
+	case INTTYPE: {
+		int64_t ** score_mat = (int64_t **)malloc(nrows * sizeof(int64_t *));
+		if (NULL == score_mat) {
+			return (matrix_t) { NULL, 0, 0, VOIDTYPE };
+		}
+
+		for (size_t i = 0; i < nrows; i++) {
+			score_mat[i] = (int64_t *)malloc(ncols * sizeof(int64_t));
+			if (NULL == score_mat[i]) {
+				for (size_t j = 0; j < i; j++) {
+					free(score_mat[j]);
+				}
+				free(score_mat);
+				return (matrix_t) { NULL, 0, 0, VOIDTYPE };
+			}
+		}
+		return (matrix_t) { (double **)score_mat, nrows, ncols, INTTYPE };
 	}
-	return (matrix_t) { score_mat, nrows, ncols };
+	}
+	return (matrix_t) { NULL, 0, 0, VOIDTYPE };
 }
 
-double find_max(const matrix_t *matrix)
+int matrix_set_value(matrix_t *matrix, element_t value)
 {
-	if (!matrix) return DBL_MAX;
-	if (!matrix->data) return DBL_MAX;
-	double score = matrix->data[0][0];
-	for (size_t i = 0; i < matrix->nrows; i++) {
-		for (size_t j = 0; j < matrix->ncols; j++) {
-			if (score < matrix->data[i][j]) {
-				score = matrix->data[i][j];
+	if (!matrix) return 0;
+	if (!matrix->ddata) return  0;
+	if (matrix->type != value.type) return 0;
+
+	switch (matrix->type) {
+	case DOUBLETYPE: {
+		for (size_t i = 0; i < matrix->nrows; i++) {
+			memcpy(matrix->ddata[i], &value.d, sizeof(double));
+		}
+		return 1;
+	}
+	case INTTYPE: {
+		for (size_t i = 0; i < matrix->nrows; i++) {
+			memcpy(matrix->idata[i], &value.i, sizeof(int64_t));
+		}
+		return 1;
+	}
+	}
+	return 0;
+}
+
+element_t find_max(const matrix_t *matrix)
+{
+	if (!matrix) return (element_t) { 0, VOIDTYPE };
+	if (!matrix->ddata) return  (element_t) { 0, VOIDTYPE };
+
+	switch (matrix->type) {
+	case DOUBLETYPE: {
+		double score = matrix->ddata[0][0];
+		for (size_t i = 0; i < matrix->nrows; i++) {
+			for (size_t j = 0; j < matrix->ncols; j++) {
+				if (score < matrix->ddata[i][j]) {
+					score = matrix->ddata[i][j];
+				}
 			}
 		}
+		return (element_t) { (double)score, DOUBLETYPE };
 	}
-	return score;
+	case INTTYPE: {
+		int64_t score = matrix->idata[0][0];
+		for (size_t i = 0; i < matrix->nrows; i++) {
+			for (size_t j = 0; j < matrix->ncols; j++) {
+				if (score < matrix->idata[i][j]) {
+					score = matrix->idata[i][j];
+				}
+			}
+		}
+		element_t ret = { 0.0, INTTYPE };
+		ret.i = score;
+		return ret;
+	}
+	}
+	return (element_t) { 0, VOIDTYPE };
 }
 
 void print_matrix(const matrix_t *matrix)
 {
 	if (!matrix) return;
-	if (!matrix->data) return;
-	for (size_t i = 0; i < matrix->nrows; i++) {
-		for (size_t j = 0; j < matrix->ncols; j++) {
-			printf(" %f", matrix->data[i][j]);
+	if (!matrix->ddata) return;
+	switch (matrix->type) {
+	case DOUBLETYPE: {
+		double score = matrix->ddata[0][0];
+		for (size_t i = 0; i < matrix->nrows; i++) {
+			for (size_t j = 0; j < matrix->ncols; j++) {
+				printf(" %f", matrix->ddata[i][j]);
+			}
+			printf(" \n");
 		}
-		printf(" \n");
+	}break;
+	case INTTYPE: {
+		int64_t score = matrix->idata[0][0];
+		for (size_t i = 0; i < matrix->nrows; i++) {
+			for (size_t j = 0; j < matrix->ncols; j++) {
+				printf(" %lld", matrix->idata[i][j]);
+			}
+			printf(" \n");
+		}
+	}
 	}
 	printf(" \n");
 }
@@ -57,13 +137,13 @@ void free_matrix(matrix_t *matrix)
 {
 	if (!matrix)
 		return;
-	if (!matrix->data)
+	if (!matrix->ddata)
 		return;
 	for (size_t i = 0; i < matrix->nrows; i++) {
-		free(matrix->data[i]);
+		free(matrix->ddata[i]);
 	}
-	free(matrix->data);
-	matrix->data = NULL;
+	free(matrix->ddata);
+	matrix->ddata = NULL;
 	matrix->nrows = 0;
 	matrix->nrows = 0;
 }
--- sw/lal_matrix.h
+++ sw/lal_matrix.h
@@ -8,21 +8,38 @@ Contact: Dmitry Sigaev <dima.sigaev@gmail.com>
 #ifndef _LAL_MARTIX_H_
 #define _LAL_MARTIX_H_
 
+#include <stdint.h>
+
+enum MATTYPE { VOIDTYPE, DOUBLETYPE, INTTYPE, FLOATTYPE };
 /**
  *  Structure of a matrix[nrows][ncols]
  *  @typedef {struct} matrix_t
  *  @field data  matrix data
- *  @field nrows the number of rows 
- *  @field ncols the number of columns 
+ *  @field nrows the number of rows
+ *  @field ncols the number of columns
  */
 typedef struct tag_matrix {
-	double **data;
+	union {
+		double **ddata;
+		int64_t **idata;
+	};
 	size_t nrows;
 	size_t ncols;
+	enum MATTYPE type;
 } matrix_t;
 
-matrix_t matrix(const size_t nrows, const size_t ncols);
-double find_max(const matrix_t *matrix);
+typedef struct tag_element {
+	union {
+		double d;
+		int64_t i;
+	};
+	enum MATTYPE type;
+} element_t;
+
+
+matrix_t matrix(const size_t nrows, const size_t ncols, enum MATTYPE type);
+element_t find_max(const matrix_t *matrix);
+int matrix_set_value(matrix_t *matrix, element_t value);
 void print_matrix(const matrix_t *matrix);
 void free_matrix(matrix_t *matrix);
 
