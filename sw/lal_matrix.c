9fe271f781c3c983c40c3570b5fec350c3ae8b28
Dmitry Sigaev <Dmitry_Sigaev@epam.com>
2016-10-17 19:51:51 +0300

    C: CHARTYPE data type add

--- sw/lal_matrix.c
+++ sw/lal_matrix.c
@@ -51,11 +51,29 @@ matrix_t matrix(const size_t nrows, const size_t ncols, enum MATTYPE type)
 		}
 		return (matrix_t) { (double **)score_mat, nrows, ncols, INTTYPE };
 	}
+	case CHARTYPE: {
+		int8_t ** score_mat = (int8_t **)malloc(nrows * sizeof(int8_t *));
+		if (NULL == score_mat) {
+			return (matrix_t) { NULL, 0, 0, VOIDTYPE };
+		}
+
+		for (size_t i = 0; i < nrows; i++) {
+			score_mat[i] = (int8_t *)malloc(ncols * sizeof(int8_t));
+			if (NULL == score_mat[i]) {
+				for (size_t j = 0; j < i; j++) {
+					free(score_mat[j]);
+				}
+				free(score_mat);
+				return (matrix_t) { NULL, 0, 0, VOIDTYPE };
+			}
+		}
+		return (matrix_t) { (double **)score_mat, nrows, ncols, CHARTYPE };
+	}
 	}
 	return (matrix_t) { NULL, 0, 0, VOIDTYPE };
 }
 
-int matrix_set_value(matrix_t *matrix, element_t value)
+int matrix_set(matrix_t *matrix, element_t value)
 {
 	if (!matrix) return 0;
 	if (!matrix->ddata) return  0;
@@ -82,6 +100,62 @@ int matrix_set_value(matrix_t *matrix, element_t value)
 					matrix->idata[i][j] = value.i;
 				}
 		}
+		return 1;
+	}
+	case CHARTYPE: {
+		for (size_t i = 0; i < matrix->nrows; i++) {
+			if (!value.i)
+				memset(matrix->cdata[i], 0, sizeof(int8_t)*matrix->ncols);
+			else
+				for (size_t j = 0; j < matrix->ncols; j++) {
+					matrix->cdata[i][j] = value.c;
+				}
+		}
+		return 1;
+	}
+	}
+	return 0;
+}
+
+int matrix_set_value(matrix_t *matrix, size_t y, size_t x, element_t value)
+{
+	if (!matrix) return 0;
+	if (!matrix->ddata) return  0;
+	if (matrix->type != value.type) return 0;
+
+	switch (matrix->type) {
+	case DOUBLETYPE: {
+		matrix->ddata[y][x] = value.d;
+		return 1;
+	}
+	case INTTYPE: {
+		matrix->idata[y][x] = value.i;
+		return 1;
+	}
+	case CHARTYPE: {
+		matrix->cdata[y][x] = value.c;
+		return 1;
+	}
+	}
+	return 0;
+}
+
+int matrix_or_bitwise(matrix_t *matrix, size_t y, size_t x, element_t value)
+{
+	if (!matrix) return 0;
+	if (!matrix->ddata) return  0;
+	if (matrix->type != value.type) return 0;
+
+	switch (matrix->type) {
+	case DOUBLETYPE: {
+		return 0;
+	}
+	case INTTYPE: {
+		matrix->idata[y][x] |= value.i;
+		return 1;
+	}
+	case CHARTYPE: {
+		matrix->cdata[y][x] |=  value.c;
 		return 1;
 	}
 	}
@@ -118,6 +192,19 @@ element_t find_max(const matrix_t *matrix)
 		ret.i = score;
 		return ret;
 	}
+	case CHARTYPE: {
+		int8_t score = matrix->cdata[0][0];
+		for (size_t i = 0; i < matrix->nrows; i++) {
+			for (size_t j = 0; j < matrix->ncols; j++) {
+				if (score < matrix->cdata[i][j]) {
+					score = matrix->cdata[i][j];
+				}
+			}
+		}
+		element_t ret = { 0.0, CHARTYPE };
+		ret.c = score;
+		return ret;
+	}
 	}
 	return (element_t) { 0, VOIDTYPE };
 }
@@ -141,6 +228,15 @@ void print_matrix(const matrix_t *matrix)
 		for (size_t i = 0; i < matrix->nrows; i++) {
 			for (size_t j = 0; j < matrix->ncols; j++) {
 				printf(" %lld", matrix->idata[i][j]);
+			}
+			printf(" \n");
+		}
+	}
+	case CHARTYPE: {
+		int8_t score = matrix->cdata[0][0];
+		for (size_t i = 0; i < matrix->nrows; i++) {
+			for (size_t j = 0; j < matrix->ncols; j++) {
+				printf(" %c", matrix->cdata[i][j]);
 			}
 			printf(" \n");
 		}
--- sw/lal_matrix.h
+++ sw/lal_matrix.h
@@ -10,7 +10,7 @@ Contact: Dmitry Sigaev <dima.sigaev@gmail.com>
 
 #include <stdint.h>
 
-enum MATTYPE { VOIDTYPE, DOUBLETYPE, INTTYPE, FLOATTYPE };
+enum MATTYPE { VOIDTYPE, DOUBLETYPE, INTTYPE, FLOATTYPE, CHARTYPE };
 /**
  *  Structure of a matrix[nrows][ncols]
  *  @typedef {struct} matrix_t
@@ -22,6 +22,7 @@ typedef struct tag_matrix {
 	union {
 		double **ddata;
 		int64_t **idata;
+		int8_t  **cdata;
 	};
 	size_t nrows;
 	size_t ncols;
@@ -32,6 +33,7 @@ typedef struct tag_element {
 	union {
 		double d;
 		int64_t i;
+		int8_t  c;
 	};
 	enum MATTYPE type;
 } element_t;
@@ -39,7 +41,9 @@ typedef struct tag_element {
 
 matrix_t matrix(const size_t nrows, const size_t ncols, enum MATTYPE type);
 element_t find_max(const matrix_t *matrix);
-int matrix_set_value(matrix_t *matrix, element_t value);
+int matrix_set(matrix_t *matrix, element_t value);
+int matrix_set_value(matrix_t *matrix, size_t y, size_t x, element_t value);
+int matrix_or_bitwise(matrix_t *matrix, size_t y, size_t x, element_t value);
 void print_matrix(const matrix_t *matrix);
 void free_matrix(matrix_t *matrix);
 
