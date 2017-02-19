dbfeb475b5fb0243bb4e7dc5644465f03f9c160f
Dmitry Sigaev <Dmitry_Sigaev@epam.com>
2017-02-19 22:06:06 +0300

    add seq2encodedseq_trans function that inserts amino-acid translation of nucleic sequence into seq structure

--- sw/lal_encoding.c
+++ sw/lal_encoding.c
@@ -7,6 +7,7 @@ Contact: Dmitry Sigaev <dima.sigaev@gmail.com>
 #include "lal_typedefs.h"
 #include "lal_report.h"
 #include "lal_encoding.h"
+#include "lal_translate_table.h"
 
 /**
 * Returns a new created sequence, with the mapped values of the input sequence.
@@ -33,6 +34,26 @@ void seq2encodedseq(sequence_t inseq, sequence_t enseq, const char* encode) {
 	enseq.seq[enseq.len] = LAL_NONEXISTENT;
 }
 
+/**
+* Returns a new created sequence, with the mapped values of the input sequence.
+* Can insert amino-acid translation of nucleic sequence into seq structure
+*
+* @param inseq   the input sequence that should be coded
+* @param enseq   the new coded sequence
+* @param encode  the encode table (nucleic triplets)
+* @param tt      the translate table (amino-acid translation)
+*/
+void seq2encodedseq_trans(sequence_t inseq, sequence_t enseq, const char* encode, translate_table_t *tt) {
+	size_t illicit_symbol = 0;
+	/* code the amino - acids from nucleic triplets */
+	for (int i = 0; i < inseq.len + 1; i++) {
+		int i2 = ((i - 1 - 2) >= 0) ? encode[inseq.seq[i - 1 - 2]] : 0;
+		int i1 = ((i - 1 - 1) >= 0) ? encode[inseq.seq[i - 1 - 1]] : 0;
+		int i0 = ((i - 1 - 0) >= 0) ? encode[inseq.seq[i - 1 - 0]] : 0;
+		enseq.seq[i] = tt->TheIntTable[i2][i1][i0];
+	}
+//	enseq.seq[enseq.len] = LAL_NONEXISTENT;
+}
 void lal_reverse(const char * source, int len, char *dest, const char *reverse_tab)
 {
 	char *ptmp;
--- sw/lal_encoding.h
+++ sw/lal_encoding.h
@@ -9,10 +9,13 @@ Contact: Dmitry Sigaev <dima.sigaev@gmail.com>
 #define _LAL_ENCODING_H_
 
 #include <stdint.h>
+#include "lal_typedefs.h"
+#include "lal_translate_table.h"
 
 #define LAL_NONEXISTENT (unsigned char)31
 
 void seq2encodedseq(sequence_t inseq, sequence_t enseq, const char* encode);
+void seq2encodedseq_trans(sequence_t inseq, sequence_t enseq, const char* encode, translate_table_t *tt);
 void lal_reverse(const char * source, int len, char *dest, const char *reverse_tab);
 
 #endif /* _LAL_ENCODING_H_ */
