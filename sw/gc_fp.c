811f751c9ee10c5e2c6cf924e2dc482236a271a4
Dmitry Sigaev <Dmitry_Sigaev@epam.com>
2017-03-06 13:13:51 +0300

    add frameplus sequenced algorithm implementation

--- sw/gc_fp.c
+++ sw/gc_fp.c
@@ -41,7 +41,7 @@ in a sequence */
 #define MS_PROFILE_MATCHTABLE_SIZE 32
 
 /* each profile line id 38 floats
-( 32 emission scores + 14 gap penalties : matchmax5 , matchmax6 , xopen , xext , fx, fopen , fext , 
+( 32 emission scores + 14 gap penalties : matchmax5 , matchmax6 , xopen , xext , fx, fopen , fext ,
 	  yopen1 , yopen2 , yopen3 , yext, xopen1 , xext1 , fx1 ;
   profile : PROF_SCORES , $matchmax5 , $matchmax6 , $xopen , $xext , $fx, $fopen , $fext ,
 	  $yopen1 , $yopen2 , $yopen3 , $yext, $xopen1 , $xext1 , $fx1 ;
@@ -49,7 +49,7 @@ in a sequence */
  */
 #define FP_PROFILE_LINE_SIZE 46
 
-/* accessor for element j in line i of profile pr */
+ /* accessor for element j in line i of profile pr */
 #define PROFILE_VAL(pr,i,j) pr[(i)*FP_PROFILE_LINE_SIZE+(j)]
 
 #define PROFILE_EMISSIONS_NO 32
@@ -154,7 +154,7 @@ static  best_t MS_Score_FP(seqtype* nseq, ms_profile_t *prof, int profLen, int s
 
 								 /* init prev_line by the scores for first line in the profile */
 
-	for (x = 0; x<seqLen; x++)
+	for (x = 0; x < seqLen; x++)
 	{
 		prev_line[x].nseq = nseq[x];
 		prev_line[x].prequal = prof->data[prev_line[x].nseq];
@@ -166,7 +166,7 @@ static  best_t MS_Score_FP(seqtype* nseq, ms_profile_t *prof, int profLen, int s
 	/* prof_line_p already set to prof->data */
 	prof_line_p = prof->data;
 
-	for (y = 1; y<profLen; y++)
+	for (y = 1; y < profLen; y++)
 	{
 		/* init sequence/state array */
 		pre = prev_line + 3; /* first 3 are empty anyway */
@@ -193,7 +193,7 @@ static  best_t MS_Score_FP(seqtype* nseq, ms_profile_t *prof, int profLen, int s
 
 
 
-		for (k = 0; k<3; k++)
+		for (k = 0; k < 3; k++)
 		{
 			xskipmatch[k] = MS_SCORE_MININF;
 			fskipmatch[k] = MS_SCORE_MININF;
@@ -399,7 +399,7 @@ best_t score_frameplus_p2n(seqtype* nseq, ms_profile_t *prof, int profLen, int s
 			const score_t match = profile(prof, j, nseq[i]);
 
 			/* depends on [i-1][j] */
-			mattr[i][j][s_fgap] = max2(mattr[i - 1][j][s_match] + f_open, 
+			mattr[i][j][s_fgap] = max2(mattr[i - 1][j][s_match] + f_open,
 				mattr[i - 1][j][s_fgap] + fext);
 
 			/* depends on [i-3][j] */
@@ -443,20 +443,20 @@ best_t score_frameplus_p2n(seqtype* nseq, ms_profile_t *prof, int profLen, int s
 
 
 #define MIN_DSP_NEGATIVE -1000
-  /*** Gencore profile ***/
-  /* min_inf in GenProf profiles.
-  This value is replaced with MS_SCORE_MININF in MS_ONE Profiles
-  Set in Gencore to (-1000) */
+/*** Gencore profile ***/
+/* min_inf in GenProf profiles.
+This value is replaced with MS_SCORE_MININF in MS_ONE Profiles
+Set in Gencore to (-1000) */
 #define MS_MIN_PROFILE_CUTOFF MIN_DSP_NEGATIVE
 
 
-  /* convert x into scoretype value sTmp1, and compute relative error:
-  if (x != 0)
-  if (x-sTmp1) != 0)
-  if (fabs((x-sTmp1)/x) > max_rel_error)
-  max_rel_error = fabs((x-sTmp1)/x)
+/* convert x into scoretype value sTmp1, and compute relative error:
+if (x != 0)
+if (x-sTmp1) != 0)
+if (fabs((x-sTmp1)/x) > max_rel_error)
+max_rel_error = fabs((x-sTmp1)/x)
 
-  */
+*/
 #define to_scoretype_(ss)  ((scoretype)rint(ss))
 
 static scoretype to_scoretype(float data, float scale, float *max_rel_error) {
@@ -566,10 +566,10 @@ double fp_gencore(const search_fp_profile_t * sp, const sequence_t * dseq, const
 				v = SCORE(k, j, 1.0, -1.0);
 			else
 				v = VDTABLE(k, j);
-//			printf(" %f, ", v);
+			//			printf(" %f, ", v);
 			PROFILE_VAL(prof_data, i/* *32+6=38*/, j) = v; //create profile line from  32 CM("A") to 38 (36-39: 1,1,1,1,0,0)
 		}
-																				  // prof_data[(i)*PROFILE_LINE_SIZE + (j)] == prof_data[(i)*38 + (j)]
+		// prof_data[(i)*PROFILE_LINE_SIZE + (j)] == prof_data[(i)*38 + (j)]
 
 		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_matchmax5) = sp->matchMax5;	//32
 		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_matchmax6) = sp->matchMax6;	//33
@@ -620,13 +620,13 @@ double fp_gencore(const search_fp_profile_t * sp, const sequence_t * dseq, const
 	}
 	free(prof_data);
 
-	int *ms_sequence = (int *)malloc(sizeof(int) * dseq->len+5);
+	int *ms_sequence = (int *)malloc(sizeof(int) * dseq->len + 5);
 	ms_sequence[0] = 13;
 	for (size_t r = 0; r < dseq->len; r++) {
-		ms_sequence[r+1] = dseq->seq[r];
+		ms_sequence[r + 1] = dseq->seq[r];
 	}
 	inpf_t max_v;
-	best_t max_s = MS_Score_FP(ms_sequence, ms_profile, prof_length, dseq->len+1);
+	best_t max_s = MS_Score_FP(ms_sequence, ms_profile, prof_length, dseq->len + 1);
 	max_v.score = ((float)(max_s.score)) * ms_profile->scaleback;
 	max_v.x = max_s.x;
 	max_v.y = max_s.y;
@@ -755,8 +755,15 @@ double fp_gencore_matrix(const search_fp_profile_t * sp, const sequence_t * dseq
 	return max_v.score;
 }
 
+
+typedef struct tag_align {
+	double score;
+	int x;
+	int y;
+} tag_align;
+
 /* optimization of matix Frame plus algorithm */
-best_t score_frameplus_p2n_opt(seqtype* nseq, ms_profile_t *prof, int profLen, int seqLen)
+tag_align score_frameplus_p2n_opt(const search_fp_profile_t * sp, const sequence_t * dseq, const sequence_t * qseq)
 {
 	/* What I know about the sequence:
 	1. before we do anything, nucleic seq. is prepened by a blank symbol.
@@ -765,103 +772,82 @@ best_t score_frameplus_p2n_opt(seqtype* nseq, ms_profile_t *prof, int profLen, i
 	*/
 	enum state_t { s_start = 0, s_fgap, s_xgap, s_ygap, s_match, s_tmpmatch, s_end, s_state_size };
 	/* Datastructure: */
-	const int seq_length = seqLen;
-	const int *translated_sequence = nseq; /* translated_sequence[i] = AA of 3 nucl in original query */
-	const int profile_len = profLen;
 
-	score_t  ***mattr;
-	static int called = 0;
+	double  ***mattr;
 
-	int i, j;
-	score_t max_score;
-	best_t max_v; /* best seen alignment score in a match state */
+	tag_align max_v = (tag_align) { 0.0, -1, -1 }; /* best seen alignment score in a match state */
 
-	mattr = malloc(seq_length * sizeof(score_t *));
+	mattr = malloc((dseq->len + 1) * sizeof(double *));
 
-	for (i = 0; i < seq_length; i++)
-	{
-		mattr[i] = malloc(profile_len * sizeof(mattr[0][0]));
-		for (j = 0; j < profile_len; j++)
+	for (size_t i = 0; i < dseq->len + 1; i++) {
+		mattr[i] = malloc((qseq->len + 1) * sizeof(mattr[0][0]));
+		for (size_t j = 0; j < (qseq->len + 1); j++)
 			mattr[i][j] = malloc(s_state_size * sizeof(mattr[0][0][0]));
 	}
 
-#define profile(prof, y, x) ( (prof)->data[ (y)*(prof)->line_size+(x) ] )
-
-	max_v.score = 0;
-	max_v.x = -1;
-	max_v.y = -1;
-
-	for (i = 0; i < seq_length; i++)
-		for (j = 0; j < profile_len; j++)
+	for (size_t i = 0; i < dseq->len + 1; i++)
+		for (size_t j = 0; j < qseq->len + 1; j++)
 			mattr[i][j][s_fgap] = mattr[i][j][s_xgap] = mattr[i][j][s_ygap] = mattr[i][j][s_match] = 0;
 
-	for (i = 3; i < seq_length; i++)
-	{
+	for (size_t i = 3; i < dseq->len + 1; i++) {
 		mattr[i][0][s_fgap] = mattr[i][0][s_xgap] = mattr[i][0][s_ygap] = mattr[i][0][s_match] = 0;
-		for (j = 1; j < profile_len; j++)
-		{
+		for (size_t j = 1; j < qseq->len + 1; j++) {
 			/* gap penalties for line n are stored in line n-1 */
 
 			/* Note: the code below was modified to read profile at [j][xopen1] instead of [j-1][xopen1]
 			the reasons are to be found out, but this seems to be the right place to read it to be consistent with om/ms runmodes */
 
-			const score_t xopen1 = profile(prof, j, MS_FRAMEPLUS_OFFSET_xopen1); /*prof_line_p[MS_FRAMEPLUS_OFFSET_xopen1]; */
-			const score_t xext1 = profile(prof, j, MS_FRAMEPLUS_OFFSET_xext1); /* prof_line_p[MS_FRAMEPLUS_OFFSET_xext1]; */
-
-			const score_t fx1 = profile(prof, j, MS_FRAMEPLUS_OFFSET_fx1);  /* prof_line_p[MS_FRAMEPLUS_OFFSET_fx1]; */
-
-																			/* go to the next profile line */
-																			/* const score_t prof_line_p = prof->data+ y * prof->line_size; */ /* prof_line_p+=prof->line_size; */
-
-			const score_t matchmax5 = profile(prof, j, MS_FRAMEPLUS_OFFSET_matchmax5); /* prof_line_p[MS_FRAMEPLUS_OFFSET_matchmax5]; */
-			const score_t matchmax6 = profile(prof, j, MS_FRAMEPLUS_OFFSET_matchmax6); /* prof_line_p[MS_FRAMEPLUS_OFFSET_matchmax6]; */
-			const score_t yopen1 = profile(prof, j, MS_FRAMEPLUS_OFFSET_yopen1); /* prof_line_p[MS_FRAMEPLUS_OFFSET_yopen1]; */
-			const score_t yopen2 = profile(prof, j, MS_FRAMEPLUS_OFFSET_yopen2); /* prof_line_p[MS_FRAMEPLUS_OFFSET_yopen2]; */
-			const score_t yopen3 = profile(prof, j, MS_FRAMEPLUS_OFFSET_yopen3); /* prof_line_p[MS_FRAMEPLUS_OFFSET_yopen3]; */
-			const score_t yext = profile(prof, j, MS_FRAMEPLUS_OFFSET_yext); /* prof_line_p[MS_FRAMEPLUS_OFFSET_yext]; */
+			const double xopen1 = sp->gapOpen;		//43
+			const double xext1 = sp->gapExt;		//44
+			const double fx1 = sp->gapFrame;		//45 latest
+			const double matchmax5 = sp->matchMax5;	//32
+			const double matchmax6 = sp->matchMax6;	//33
+			const double yopen1 = sp->gapOpen;		//39
+			const double yopen2 = sp->gapOpen2;		//40
+			const double yopen3 = sp->gapOpen3;		//41
+			const double yext = sp->gapExt;			//42
 																			 /* constant */
-			const score_t fext = profile(prof, j, MS_FRAMEPLUS_OFFSET_fext); /* prof_line_p[MS_FRAMEPLUS_OFFSET_fext]; */
-			const score_t f_open = profile(prof, j, MS_FRAMEPLUS_OFFSET_fopen); /* prof_line_p[MS_FRAMEPLUS_OFFSET_fopen]; */ /* fopen is a std. function name too ? */
+			const score_t fext = sp->gapFrameExt;		//38
+			const score_t f_open = sp->gapFrameOpen;	//37
+			double v;
+			if (!sp->mtx)
+				v = SCORE(qseq->seq[j - 1], dseq->seq[i - 1], 1.0, -1.0);
+			else
+				v = VDTABLE(qseq->seq[j - 1], dseq->seq[i - 1]);
 
-			const score_t match = profile(prof, j, nseq[i]);
+			const double match = v;
 
 			/* depends on [i-1][j] */
-			mattr[i][j][s_fgap] = max2(mattr[i - 1][j][s_match] + f_open,
-				mattr[i - 1][j][s_fgap] + fext);
+			mattr[i][j][s_fgap] = MAX(mattr[i - 1][j][s_match] + f_open, mattr[i - 1][j][s_fgap] + fext);
 
 			/* depends on [i-3][j] */
-			mattr[i][j][s_xgap] = max3(mattr[i - 3][j][s_xgap] + xext1,
-				mattr[i - 3][j][s_match] + xopen1,
+			mattr[i][j][s_xgap] = MAX(MAX(mattr[i - 3][j][s_xgap] + xext1, mattr[i - 3][j][s_match] + xopen1),
 				/*	    xopen1,  -- match above is non-negative*/ /* from s_start */
 				mattr[i - 3][j][s_fgap] + fx1);
 			/* depends on [i-2..i][j-1] */
-			mattr[i][j][s_ygap] = max5(mattr[i - 1][j - 1][s_match] + yopen2,
-				mattr[i][j - 1][s_match] + yopen1,
-				mattr[i - 2][j - 1][s_match] + yopen3,
-				mattr[i][j - 1][s_ygap] + yext,
+			mattr[i][j][s_ygap] = MAX(MAX(MAX(MAX(mattr[i - 1][j - 1][s_match] + yopen2,
+				mattr[i][j - 1][s_match] + yopen1),
+				mattr[i - 2][j - 1][s_match] + yopen3),
+				mattr[i][j - 1][s_ygap] + yext),
 				0 /*from s_start */);
 			/* depends on [i-3][j-1] */
-			mattr[i][j][s_match] = match + max5(mattr[i - 3][j - 1][s_fgap],
-				mattr[i - 3][j - 1][s_xgap],
-				mattr[i - 3][j - 1][s_ygap],
-				mattr[i - 3][j - 1][s_match],
+			mattr[i][j][s_match] = match + MAX(MAX(MAX(MAX(mattr[i - 3][j - 1][s_fgap],
+				mattr[i - 3][j - 1][s_xgap]),
+				mattr[i - 3][j - 1][s_ygap]),
+				mattr[i - 3][j - 1][s_match]),
 				0);
-			mattr[i][j][s_match] = max4(0,
-				mattr[i][j][s_match],
-				mattr[i - 2][j - 1][s_match] + matchmax5,
+			mattr[i][j][s_match] = MAX(MAX(MAX(0,
+				mattr[i][j][s_match]),
+				mattr[i - 2][j - 1][s_match] + matchmax5),
 				mattr[i - 1][j - 1][s_match] + matchmax6);
 
 			if (max_v.score < mattr[i][j][s_match])
-			{
-				max_v.score = mattr[i][j][s_match];
-				max_v.x = i;
-				max_v.y = j;
-			}
+				max_v = (tag_align) { mattr[i][j][s_match], i, j };
 		}
 	}
-	for (i = 0; i < seq_length; i++)
+	for (size_t i = 0; i < dseq->len + 1; i++)
 	{
-		for (j = 0; j < profile_len; j++)
+		for (size_t j = 0; j < qseq->len + 1; j++)
 			free(mattr[i][j]);
 		free(mattr[i]);
 	}
@@ -870,119 +856,7 @@ best_t score_frameplus_p2n_opt(seqtype* nseq, ms_profile_t *prof, int profLen, i
 
 double fp_gencore_seq(const search_fp_profile_t * sp, const sequence_t * dseq, const sequence_t * qseq)
 {
-	/* model->profile_line_size = 66;  len of raw prof*/
-	//MS_gcprofile_parts_bounds(model, &read_base, &line_size); gets own frame plus profile from model file
-	int read_base = 20; /*shift in raw profile*/
-	int line_size = 46; /*32 matrix + 14 gaps*/
-	float *data_from;
-	scoretype *data_to;
-	float scale = sp->mtx->scale;
-	float max_rel_error = 0;
-	ms_profile_t * ms_profile = malloc(sizeof(ms_profile_t));
-	ms_profile->scaleback = 1.0 / sp->mtx->scale;
-	ms_profile->line_size = line_size;
-	int prof_length = qseq->len + 1; // INT_MIN + seq(i-1)
-	int prf_len = (prof_length)* FP_PROFILE_LINE_SIZE * sizeof(float);
-	float *prof_data = (float *)malloc(prf_len);
-
-	for (size_t j = 0; j < PROFILE_EMISSIONS_NO; j++)
-		PROFILE_VAL(prof_data, 0/* *32+6=38*/, j) = INT_MIN; /*sigaev: global aligment*/
-
-															 /* gap penalties copied from first line */
-	prof_data[MS_FRAMEPLUS_OFFSET_matchmax5] = sp->matchMax5;	//32
-	prof_data[MS_FRAMEPLUS_OFFSET_matchmax6] = sp->matchMax6;	//33
-	prof_data[MS_FRAMEPLUS_OFFSET_xopen1] = sp->gapOpen;		//43
-	prof_data[MS_FRAMEPLUS_OFFSET_xext1] = sp->gapExt;			//44
-	prof_data[MS_FRAMEPLUS_OFFSET_fopen] = sp->gapFrameOpen;	//37
-	prof_data[MS_FRAMEPLUS_OFFSET_fext] = sp->gapFrameExt;		//38
-	prof_data[MS_FRAMEPLUS_OFFSET_yopen1] = sp->gapOpen;		//39
-	prof_data[MS_FRAMEPLUS_OFFSET_yopen2] = sp->gapOpen2;		//40
-	prof_data[MS_FRAMEPLUS_OFFSET_yopen3] = sp->gapOpen3;		//41
-	prof_data[MS_FRAMEPLUS_OFFSET_yext] = sp->gapExt;			//42
-	prof_data[MS_FRAMEPLUS_OFFSET_fx1] = sp->gapFrame;			//45 latest
-																/* @@ next 3 fields are not used */
-																//	MS_FRAMEPLUS_OFFSET_xopen, MS_FRAMEPLUS_OFFSET_xext, MS_FRAMEPLUS_OFFSET_fx,
-	prof_data[MS_FRAMEPLUS_OFFSET_xopen] = 0.0;					//34
-	prof_data[MS_FRAMEPLUS_OFFSET_xext] = 0.0;					//35
-	prof_data[MS_FRAMEPLUS_OFFSET_fx] = 0.0;					//35
-
-
-	for (size_t i = 1; i < prof_length; i++) {
-		int k = qseq->seq[i - 1];
-		for (size_t j = 0; j < PROFILE_EMISSIONS_NO; j++) { //#define PROFILE_EMISSIONS_NO 32
-			double v;
-			if (!sp->mtx)
-				v = SCORE(k, j, 1.0, -1.0);
-			else
-				v = VDTABLE(k, j);
-			//			printf(" %f, ", v);
-			PROFILE_VAL(prof_data, i/* *32+6=38*/, j) = v; //create profile line from  32 CM("A") to 38 (36-39: 1,1,1,1,0,0)
-		}
-		// prof_data[(i)*PROFILE_LINE_SIZE + (j)] == prof_data[(i)*38 + (j)]
-
-		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_matchmax5) = sp->matchMax5;	//32
-		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_matchmax6) = sp->matchMax6;	//33
-		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_xopen1) = sp->gapOpen;		//43
-		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_xext1) = sp->gapExt;			//44
-		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_fopen) = sp->gapFrameOpen;	//37
-		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_fext) = sp->gapFrameExt;		//38
-		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_yopen1) = sp->gapOpen;		//39
-		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_yopen2) = sp->gapOpen2;		//40
-		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_yopen3) = sp->gapOpen3;		//41
-		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_yext) = sp->gapExt;			//42
-		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_fx1) = sp->gapFrame;			//45 latest
-																					/* @@ next 3 fields are not used */
-																					//	MS_FRAMEPLUS_OFFSET_xopen, MS_FRAMEPLUS_OFFSET_xext, MS_FRAMEPLUS_OFFSET_fx,
-		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_xopen) = 0.0;					//34
-		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_xext) = 0.0;					//35
-		PROFILE_VAL(prof_data, i, MS_FRAMEPLUS_OFFSET_fx) = 0.0;					//35
-	}
-
-	ms_profile->data = (scoretype *)malloc(sizeof(scoretype) * ms_profile->line_size * prof_length);
-
-	{
-		FILE *output;
-		char strbuf[200];
-		sprintf(strbuf, "%s_%0.5d", "soft_api.dbg.a.", 0);
-		output = fopen(strbuf, "w");
-
-		for (size_t i = 0; i < prof_length; i++) { /* for each profile line */
-
-												   /* First model->max_dprf profile lines are not actual lines but are added
-												   so that algorithm could go beyond the boundary and first lines would
-												   not be special cases. MS computes first lines separately. */
-			data_from = &(prof_data[(i + 0/*0== SoftInfo_g.model->max_dprf*/) * FP_PROFILE_LINE_SIZE]);
-			data_to = &(ms_profile->data[i*ms_profile->line_size]);
-
-			for (size_t j = 0; j < line_size; j++) { /* for each position in the profile */
-													 /*float mre = max_rel_error;/-* debug */
-				data_to[j] = get_scaled(data_from[j], scale, &max_rel_error);
-				fprintf(output, "i= %zd j= %zd %f * %f -> %d\n", i, j, (double)data_from[j], scale, data_to[j]);
-				//						printf(": %d %d data=%.10f mre=%.10f\n",i,j,data_from[j],max_rel_error);
-				//					if (mre != max_rel_error)
-				//							printf("DEBUG: MS_gcprofile2msone_profile(): %d %d data=%.10f mre=%.10f\n",i,j,data_from[j],max_rel_error);
-			} /* for(j=0;j<line_size;j++) */
-
-		} /* for(i=0;i<prof_length;i++) */
-
-		fclose(output);
-	}
-	free(prof_data);
-
-	int *ms_sequence = (int *)malloc(sizeof(int) * dseq->len + 5);
-	ms_sequence[0] = 13;
-	for (size_t r = 0; r < dseq->len; r++) {
-		ms_sequence[r + 1] = dseq->seq[r];
-	}
-	inpf_t max_v;
-	best_t max_s = score_frameplus_p2n_opt(ms_sequence, ms_profile, prof_length, dseq->len + 1);
-	max_v.score = ((float)(max_s.score)) * ms_profile->scaleback;
-	max_v.x = max_s.x;
-	max_v.y = max_s.y;
-	max_v.state = -1; /* unknown */
-	free(ms_sequence);
-	free(ms_profile->data);
-	free(ms_profile);
-	return max_v.score;
+	tag_align max_s = score_frameplus_p2n_opt(sp, dseq, qseq);
+	return max_s.score;
 }
 
